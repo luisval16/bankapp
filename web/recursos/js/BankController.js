98f767fcf2b286d0142e2ab5af732550745cf0e7
dell <dell@dell-PC>
2018-02-26 08:57:26 -0500

    bank app

--- web/recursos/js/BankController.js
+++ web/recursos/js/BankController.js
@@ -19,13 +19,26 @@ app.controller('bankCtrl', function ($scope, $interval) {
     ];
     $scope.showTable = true;
     $scope.showEdit = false;
+    $scope.showConfirm = false;
+    $scope.showForm = false;
+    $scope.operacion = '';
+    $scope.opcion = '';
+    //Variables de paginacion
+    $scope.currentPage = 1;
+    $scope.totalItems = $scope.personas.length;
+    $scope.entryLimit = 5;
+    $scope.noOfPages = Math.ceil($scope.totalItems / $scope.entryLimit);
+    
+    //fin Variables de paginacion
     $scope.persona = {id:0,nombre:'',genero:'',nacionalidad:''};
     //Fin inicializacion de variables
     
     
-    //Inicio Funciones
+    /*       Inicio 
+            Funciones      */
     $scope.editUser = function(id){
         $scope.showTable = false;
+        $scope.showForm = true;
         if (id == 'new'){
             $scope.persona = {id:0,nombre:'',genero:'',nacionalidad:''};
             $scope.showEdit = false;
@@ -41,17 +54,82 @@ app.controller('bankCtrl', function ($scope, $interval) {
     
     $scope.goBack = function(){
         $scope.showTable = true;
+        $scope.showConfirm = false;
+         $scope.showForm = false;
         $scope.persona = {id:0,nombre:'',genero:'',nacionalidad:''};
     };
     
+    $scope.valUser= function(op){
+     $scope.showConfirm = true;
+     $scope.showForm = false;
+        if (op == 'a') {
+            $scope.operacion = 'Agregar';
+            $scope.opcion = 'a';
+        } else if (op == 'm') {
+            $scope.operacion = 'Modificar';
+            $scope.opcion = 'm';
+        } else if (op == 'e') {
+            $scope.operacion = 'Eliminar';
+            $scope.opcion = 'e';
+        }
+    };
+    
+    $scope.executeOp= function(){
+     $scope.showConfirm = true;
+     $scope.showForm = false;
+        if ($scope.opcion == 'a') {
+            $scope.addUser();
+        } else if ($scope.opcion == 'm') {
+            $scope.modUser();
+        } else if ($scope.opcion == 'e') {
+            $scope.delUser();
+        }
+    };
+    
+    
     $scope.addUser= function(){
-        $scope.persona.id = $scope.personas[$scope.personas.length - 1].id + 1;
+        $scope.showConfirm = false;
+        $scope.showTable = true;
+        if ($scope.personas.length == 0) {
+           $scope.persona.id = 1; 
+        }else{
+           $scope.persona.id = $scope.personas[$scope.personas.length - 1].id + 1; 
+        }
         $scope.personas.push($scope.persona);
         $scope.persona = {id:0,nombre:'',genero:'',nacionalidad:''};
         $scope.showTable = true;
     };
     
-    //Fin Funciones
+    $scope.modUser=function(){
+        $scope.showConfirm = false;
+        $scope.showTable = true;
+        for (var i =0;i<$scope.personas.length;i++){
+            if ($scope.personas[i].id == $scope.persona.id) {
+                $scope.personas[i] = $scope.persona;
+                
+            }
+        }
+        
+        $scope.showTable = true;
+        $scope.persona = {id:0,nombre:'',genero:'',nacionalidad:''};
+    };
+    
+    $scope.delUser=function(){
+        $scope.showConfirm = false;
+        $scope.showTable = true;
+        for (var i =0;i<$scope.personas.length;i++){
+            if ($scope.personas[i].id == $scope.persona.id) {
+                $scope.personas.splice(i,1);
+                
+            }
+        }
+        
+        $scope.showTable = true;
+        $scope.persona = {id:0,nombre:'',genero:'',nacionalidad:''};
+    };
+    
+    /*      Fin 
+     *      Funciones   */
 });
 
 
